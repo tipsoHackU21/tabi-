e9dcf646ad5b0e7db2b3481170f6d7718914d652
fujimotoayaka <fujimotoayaka.happy@gmail.com>
2021-03-22 01:40:38 +0900

    概要のswiftファイルの追加

---
+++ tabi!/account_2.swift
@@ -0,0 +1,18 @@
+//
+//  account_2.swift
+//  tabi!
+//
+//  Created by 藤元彩花 on 2021/03/22.
+//
+
+import Foundation
+import FirebaseAuth
+import UIKit
+
+class account_2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
