b10c313f73bf11798d49c79178566be101581f75
imiinz1331 <imiinz1331@gmail.com>
2021-03-26 16:15:21 +0900

    mypageEdit追加

--- tabi!/mypageEdit.swift
+++ tabi!/mypageEdit.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class mypageEdit: UIViewController {
+    
+    @IBOutlet weak var username: UITextView!
+    @IBOutlet weak var usermessage: UITextView!
+    @IBOutlet weak var updateButton: UIButton!
+
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        username.layer.borderColor = UIColor.blue.cgColor
+        username.layer.borderWidth = 5.0
+        usermessage.layer.borderColor = UIColor.blue.cgColor
+        usermessage.layer.borderWidth = 5.0
+    
+    }
+    @IBAction func TapButton(_ sender: Any) {
+        //ここにプロフィール更新記述処理
+        
+        
+        //更新後にViewを破棄して戻る
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+        
